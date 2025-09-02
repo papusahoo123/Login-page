429d5666add0945a2bb83ab4f22af1bec42b5b77
Your Name <yourgmail@gmail.com>
2025-09-02 20:39:06 +0530

    add file

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -16,31 +16,54 @@ function Signup() {
       console.log(error)
     }
   }
-    return (<div>
-        <form onSubmit={handleSumbit}>
-     <div className='mb-3 mt-5'>
-        <label for="exampleInputEmail1" class="form-label">Name</label>
-        <input type="email" onChange={(e) => setName(e.target.value)} placeholder='Enter Username' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />   
+  return ( 
+<div> 
+
+<div className='mb-3 mt-5'>
+  <label htmlFor="exampleInputName" className="form-label">Name</label>
+  <input
+    type="text"
+    onChange={(e) => setName(e.target.value)}
+    placeholder='Enter Username'
+    className="form-control"
+    id="exampleInputName"
+    aria-describedby="nameHelp"
+  />
+</div>
+
+<div className="mb-3 mt-4">
+  <label htmlFor="exampleInputEmail" className="form-label">Email address</label>
+  <input
+    type="email"
+    onChange={(e) => setEmail(e.target.value)}
+    placeholder='Email'
+    className="form-control"
+    id="exampleInputEmail"
+    aria-describedby="emailHelp"
+  />
+  <div id="emailHelp" className="form-text">
+    We'll never share your email with anyone else.
   </div>
-  <div class="mb-3 mt-4">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" onChange={(e) =>  setEmail(e.target.value)} placeholder='Email' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" onChange={(e) =>  setPassword(e.target.value)} class="form-control" id="exampleInputPassword1" />
-  </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Signup</button>
-  <div className='mt-4'>
-  <p>Already Have Account? <a href='@'>Login</a></p>
-  </div>
-</form>
-    </div>);
+</div>
+
+<div className="mb-3">
+  <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+  <input
+    type="password"
+    onChange={(e) => setPassword(e.target.value)}
+    className="form-control"
+    id="exampleInputPassword1"
+  />
+</div>
+
+<div className="mb-3 form-check">
+  <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+  <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+</div>
+</div>
+);
 }
 
 export default Signup;
+
+ 
